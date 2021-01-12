0947f8c55e34c7d89e4c03b6d0f7bf66b4c48675
LeongYuXuan <S10204536@connect.np.edu.sg>
2021-01-12 23:41:59 +0800

    Partial Add eventListener for submit pic

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
+// Test code to check event listener
 $("#submit").click(function(event){
     event.preventDefault();
     let test = "<p>Test<p>";
     $("body").append(test);
 });
+
+// Replace placeholder img with submitted image
+$('aniScreen').on("click",function(){
+    let img = $('#aniScreen').value();
+    console.log(img)
+
+})
