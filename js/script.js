34db87e409650e8be71f2547669f2a5932a624db
LeongYuXuan <S10204536@connect.np.edu.sg>
2021-01-13 22:58:18 +0800

    Add leap of faith js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,33 @@
 // Test code to check event listener
-$("#submit").click(function(event){
+$("#submit").on("click",function(event){
     event.preventDefault();
     let test = "<p>Test<p>";
     $("body").append(test);
 });
 
-// Replace placeholder img with submitted image
-$('aniScreen').on("click",function(){
-    let img = $('#aniScreen').value();
-    console.log(img)
-    //Redp
+// Replace placeholder img with submitted image (TBA)
+//$('aniScreen').on("click",function(){
+    //let img = $('#aniScreen').value();
+    //console.log(img)
+//})
 
-})
+// //leap of faith event listener for uploading image
+// var form = new FormData();
+// form.append("image", "R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7");
+
+// var settings = {
+//   "url": "https://api.imgur.com/3/image",
+//   "method": "POST",
+//   "timeout": 0,
+//   "headers": {
+//     "Authorization": "Client-ID {{clientId}}"
+//   },
+//   "processData": false,
+//   "mimeType": "multipart/form-data",
+//   "contentType": false,
+//   "data": form
+// };
+
+// $.ajax(settings).done(function (response) {
+//   console.log(response);
+// });
